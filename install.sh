2df477d0195019de23413ea76b34ee41ee2e37c8
bolu61 <bolu61@zjc.dev>
2022-08-15 17:16:45 -0400

    update install.sh to allow backup

--- install.sh
+++ install.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
+set -e
+if [-d "$HOME/dotfiles"] then
+    DATA_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/dotfiles"
+    mkdir -p "$DATA_DIR"
+    mv "$HOME/dotfiles" "$DATA_DIR/backup"
+fi
+
+git clone --bare https://github.com/bolu61/dotfiles "$HOME/dotfiles"
 alias dotfiles='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
-git clone https://github.com/bolu61/dotfiles "$HOME/dotfiles"
-dotfiles checkout -f
+dotfiles checkout 
+if [ $? = 0 ]; then
+    echo "checked out config"
+else
+    echo "backing up pre-existing dot files"
+    mkdir -p "$HOME/dotfiles.b"
+    ls-tree --name-only --full-tree -r HEAD | xargs -I{} mv {} "$HOME/dotfiles.b"
+    dotfiles checkout
+    echo "checked out config"
+fi
+dotfiles config status.showUntrackedFiles no
