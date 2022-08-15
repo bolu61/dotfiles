92ebdc0e455414d39a9e066b60ec3aefbb57d5a0
bolu61 <bolu61@zjc.dev>
2022-08-15 18:49:17 -0400

    change backup dir

--- install.sh
+++ install.sh
@@ -6,8 +6,7 @@ SOURCE="${SOURCE:-"."}"
 git clone --bare "$SOURCE" "$HOME/.dotfiles"
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 echo "backing up pre-existing dot files"
-mkdir -p "$HOME/dotfiles.b"
-dotfiles ls-tree --name-only --full-tree -r HEAD | xargs -I% sh -c "[ ! -f $HOME/% ] || mv $HOME/% $HOME/dotfiles.b"
+dotfiles ls-tree --name-only --full-tree -r HEAD | xargs -I% sh -c "[ ! -f $HOME/% ] || mkdir -p "$HOME/.dotfiles.b" && mv $HOME/% $HOME/.dotfiles.b"
 dotfiles checkout
 echo "checked out config"
 dotfiles config status.showUntrackedFiles no
