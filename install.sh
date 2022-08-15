ef590f09a61fea19ca0791d4e7fad040ab7c588b
bolu61 <bolu61@zjc.dev>
2022-08-15 18:42:01 -0400

    update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 set -e
-if [ -d "$HOME/dotfiles" ]; then
-    DATA_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/dotfiles"
-    mkdir -p "$DATA_DIR"
-    rm -rf "$DATA_DIR/backup"
-    mv "$HOME/dotfiles" "$DATA_DIR/backup"
-fi
 
-git clone --bare https://github.com/bolu61/dotfiles "$HOME/dotfiles"
-alias dotfiles='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+SOURCE="${SOURCE:-"."}"
+
+git clone --bare "$SOURCE" "$HOME/.dotfiles"
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 echo "backing up pre-existing dot files"
 mkdir -p "$HOME/dotfiles.b"
 dotfiles ls-tree --name-only --full-tree -r HEAD | xargs -I% sh -c "[ ! -f $HOME/% ] || mv $HOME/% $HOME/dotfiles.b"
