4e423de9a0e91ce3c5a38e0fb5450a63b19cc893
bolu61 <bolu61@zjc.dev>
2022-08-15 18:08:53 -0400

    fix backup

--- install.sh
+++ install.sh
@@ -8,14 +8,9 @@ fi
 
 git clone --bare https://github.com/bolu61/dotfiles "$HOME/dotfiles"
 alias dotfiles='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
-dotfiles checkout 
-if [ $? = 0 ]; then
-    echo "checked out config"
-else
-    echo "backing up pre-existing dot files"
-    mkdir -p "$HOME/dotfiles.b"
-    ls-tree --name-only --full-tree -r HEAD | xargs -I{} mv {} "$HOME/dotfiles.b"
-    dotfiles checkout
-    echo "checked out config"
-fi
+echo "backing up pre-existing dot files"
+mkdir -p "$HOME/dotfiles.b"
+ls-tree --name-only --full-tree -r HEAD | xargs -I{} sh -c "[ ! -f {} ] && mv {} $HOME/dotfiles.b"
+dotfiles checkout
+echo "checked out config"
 dotfiles config status.showUntrackedFiles no
