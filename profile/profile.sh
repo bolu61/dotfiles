19ade9e38d5b7d0865f1dc80d583168c240727d0
bolu61 <bolu61@zjc.dev>
2023-04-04 19:06:43 -0400

    use nullglob for profiles

--- profile/profile.sh
+++ profile/profile.sh
@@ -1,6 +1,6 @@
 # ~/.profile: executed by the command interpreter for login shells.
-# This file is not read by bash(1), if ~/.bash_profile or ~/.bash_login
-# exists.
+
+shopt -s nullglob
 
 # Set DOTFILES installed modules
 export DOTFILESMODULES="{{DOTFILESMODULES}}"
