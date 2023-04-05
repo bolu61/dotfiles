8242caedfd25338dc8b7ae1d4a242e72fc27ddde
bolu61 <bolu61@zjc.dev>
2023-04-05 10:42:39 -0400

    change to fern tree viewer

--- nvim/lua/plugins/tree.lua
+++ nvim/lua/plugins/tree.lua
@@ -1,40 +1,9 @@
 return {
   {
-    "nvim-neo-tree/neo-tree.nvim",
+    "lambdalisue/fern.vim",
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    },
-    cmd = "Neotree",
-    keys = {
-      {
-        "<leader>fe",
-        function()
-          require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
-        end,
-        desc = "Explorer NeoTree (cwd)",
-      },
-      { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (cwd)", remap = true },
-    },
-    deactivate = function()
-      vim.cmd([[Neotree close]])
-    end,
-    init = function()
-      vim.g.neo_tree_remove_legacy_commands = 1
-      if vim.fn.argc() == 1 then
-        local stat = vim.loop.fs_stat(vim.fn.argv(0))
-        if stat and stat.type == "directory" then
-          require("neo-tree")
-        end
-      end
-    end,
-    opts = {
-      window = {
-        mappings = {
-          ["<space>"] = "none",
-        },
-      },
-    },
+      "lambdalisue/fern-git-status.vim",
+      "lambdalisue/fern-hijack.vim",
+    }
   },
 }
