1c4c2dfb33bbcf122d437b79abd83a633e1b500f
bolu61 <bolu@bolupi.me>
2023-04-05 10:42:14 -0400

    fix snippets

--- nvim/lua/plugins/snippets.lua
+++ nvim/lua/plugins/snippets.lua
@@ -3,10 +3,10 @@ return {
     "L3MON4D3/LuaSnip",
     dependencies = {
       "rafamadriz/friendly-snippets",
-      config = function()
-        require("luasnip.loaders.from_vscode").lazy_load()
-      end,
     },
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
     opts = {
       history = true,
       delete_check_events = "TextChanged",
