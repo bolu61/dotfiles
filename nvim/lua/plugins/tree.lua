7f1de558343a82267cace117a9bed7afa6ea2d63
bolu61 <bolu61@zjc.dev>
2023-04-05 13:18:38 -0400

    add keymapping to open file explorer

--- nvim/lua/plugins/tree.lua
+++ nvim/lua/plugins/tree.lua
@@ -3,7 +3,16 @@ return {
     "lambdalisue/fern.vim",
     dependencies = {
       "lambdalisue/fern-git-status.vim",
-      "lambdalisue/fern-hijack.vim",
+      
+    },
+    keys = {
+      { "<leader><tab>", "<cmd>Fern . -reveal=%<cr>", "Open file picker."},
+    },
+  },
+  {
+    "lambdalisue/fern-hijack.vim", 
+    dependencies = {
+      "lambdalisue/fern.vim"
     }
   },
 }
