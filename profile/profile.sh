d8aa6628ca93b6a90d4711588c2a65568e67743c
bolu <bolu@bolupi.me>
2023-10-01 02:52:40 -0400

    update

--- profile/profile.sh
+++ profile/profile.sh
@@ -1,14 +1,9 @@
-# ~/.profile: executed by the command interpreter for login shells.
-
 shopt -s nullglob
 
-# Set DOTFILES installed modules
 export DOTFILESMODULES="{{DOTFILESMODULES}}"
 
-# Set PATH so it includes user's private bin.
 export PATH="$HOME/.local/bin:$PATH"
 
-# Define XDG directories.
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
