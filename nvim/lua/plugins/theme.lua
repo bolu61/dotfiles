8bf93c1fe3975ab1b72fa354a3bf1a84fd2b59f9
bolu61 <bolu@bolupi.me>
2024-01-16 17:14:27 +0000

    change nvim background

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -8,10 +8,15 @@ return {
     opts = {
       flavour = "frappe",
       no_italic = true,
+      background = {
+        light = "latte",
+        dark = "frappe",
+      },
     },
     config = function(_, opts)
       require("catppuccin").setup(opts)
       vim.cmd([[colorscheme catppuccin]])
+      vim.o.background = "dark"
     end,
   },
 }
