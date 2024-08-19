3d06bb57c27649dbe6498f45ea53e2c42cb39cd2
bolu61 <bolu61@zjc.dev>
2024-08-19 12:30:04 +0900

    update mason

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -24,6 +24,7 @@ return {
             settings = {
               python = {
                 analysis = {
+                  typeCheckingMode = 'strict',
                   useLibraryCodeForTypes = false,
                 }
               }
