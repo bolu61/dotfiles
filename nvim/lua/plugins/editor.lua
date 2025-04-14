a1d721b59dea6a45acdebc22c599e4e3cb198cd1
bolu61 <bolu61@zjc.dev>
2025-04-14 13:56:15 -0400

    add telescope keys

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -361,5 +361,11 @@ return {
 	{
 		'nvim-telescope/telescope.nvim',
 		dependencies = { 'nvim-lua/plenary.nvim' },
+		keys = {
+			{"<leader>ft", function() require("telescope.builtin").find_files() end, desc = "Telescope find files"},
+			{"<leader>fg", function() require("telescope.builtin").live_grep() end, desc = "Telescope live grep"},
+			{"<leader>fb", function() require("telescope.builtin").buffers() end, desc = "Telescope buffers"},
+			{"<leader>fh", function() require("telescope.builtin").help_tags() end, desc = "Telescope help tags"},
+		}
 	}
 }
